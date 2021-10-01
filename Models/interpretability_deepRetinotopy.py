a052c0500de75b91d2f82f67f130e08088059c96
felenitaribeiro <felenita.ribeiro@gmail.com>
2021-10-01 14:25:11 +1000

    neighborhood size

--- Models/interpretability_deepRetinotopy.py
+++ Models/interpretability_deepRetinotopy.py
@@ -25,7 +25,7 @@ test_dataset = Retinotopy(path, 'Test', transform=T.Cartesian(),
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
 nodes = np.load('nodes_earlyVisualCortex.npz')['list']
-neighborhood_size = 9
+neighborhood_size = 15
 
 
 # Model

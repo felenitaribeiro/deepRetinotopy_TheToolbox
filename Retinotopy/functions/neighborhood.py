62715c19e333839a9c4e3bee4397af211d127c81
felenitaribeiro <felenita.ribeiro@gmail.com>
2021-10-08 11:20:06 +1000

    neighborhood reversed

--- Retinotopy/functions/neighborhood.py
+++ Retinotopy/functions/neighborhood.py
@@ -1,6 +1,5 @@
 import torch
-
-
+import numpy as np
 
 def node_neighbourhood(data, node, neighborhood_size):
     if neighborhood_size == 1:
@@ -47,3 +46,45 @@ def node_neighbourhood(data, node, neighborhood_size):
             [0.027303819, 1.4386905])
     return data, neighbors
 
+
+def node_neighbourhood_reverse(data, node, neighborhood_size):
+    if neighborhood_size == 1:
+        neighbor_nodes = data.edge_index[1][
+            data.edge_index[0] == torch.tensor([int(node)])]
+        neighbor_nodes = torch.cat((neighbor_nodes, torch.tensor([int(node)])))
+        neighbors = {'level_1': neighbor_nodes}
+
+        # constant
+        kernel = neighbors['level_1']
+        ROIminusPatch = [item for item in np.arange(0, 3267) if
+                         item not in kernel]
+        data.x[ROIminusPatch] = torch.ones(
+            data.x[ROIminusPatch].shape) * torch.tensor(
+            [0.027303819, 1.4386905])
+
+    else:
+        neighbor_nodes = data.edge_index[1][
+            data.edge_index[0] == torch.tensor([int(node)])]
+        neighbor_nodes = torch.cat((neighbor_nodes,  torch.tensor([int(node)])))
+        neighbors = {'level_' + str(1): neighbor_nodes}
+
+        for l in range(neighborhood_size - 1):
+            all_neighbors = torch.tensor([], dtype=torch.long)
+            for n in range(len(neighbors[
+                                   'level_' + str(l + 1)])):
+                all_neighbors = torch.cat(
+                    (all_neighbors, data.edge_index[1][
+                        data.edge_index[0] == neighbors[
+                            'level_' + str(l + 1)][n]]))
+
+            neighbors['level_' + str(l + 2)] = torch.unique(torch.cat(
+                (neighbors['level_' + str(l + 1)], all_neighbors)))
+
+        # # constant
+        kernel = neighbors['level_' + str(neighborhood_size)]
+        ROIminusPatch = [item for item in np.arange(0, 3267) if
+                         item not in kernel]
+        data.x[ROIminusPatch] = torch.ones(
+            data.x[ROIminusPatch].shape) * torch.tensor(
+            [0.027303819, 1.4386905])
+    return data, neighbors

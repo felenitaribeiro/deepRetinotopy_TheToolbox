normalization fix

Models/deepRetinotopy_updated.py
@@ -13,13 +13,14 @@ from torch_geometric.data import DataLoader
 from torch_geometric.nn import SplineConv
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'Retinotopy', 'data')
-
+norm_value = 70.4237
 pre_transform = T.Compose([T.FaceToEdge()])
-train_dataset = Retinotopy(path, 'Train', transform=T.Cartesian(max_value=10),
+
+train_dataset = Retinotopy(path, 'Train', transform=T.Cartesian(max_value=norm_value),
                            pre_transform=pre_transform, n_examples=181,
                            prediction='polarAngle', myelination=True,
                            hemisphere='Left') # Change to Right for the RH
-dev_dataset = Retinotopy(path, 'Development', transform=T.Cartesian(max_value=10),
+dev_dataset = Retinotopy(path, 'Development', transform=T.Cartesian(max_value=norm_value),
                          pre_transform=pre_transform, n_examples=181,
                          prediction='polarAngle', myelination=True,
                          hemisphere='Left') # Change to Right for the RH
@@ -212,7 +213,7 @@ for i in range(5):
     # Saving model's learned parameters
     torch.save(model.state_dict(),
                osp.join(osp.dirname(osp.realpath(__file__)), 'output',
-                        'deepRetinotopy_PA_LH_model' + str(i+1) + '_fixNorm.pt')) # Rename if RH
+                        'deepRetinotopy_PA_LH_model' + str(i+1) + '.pt')) # Rename if RH
 
 # end = time.time() # To find out how long it takes to train the model
 # time = (end - init) / 60

Models/generalize_deepRetinotopy.py
@@ -14,17 +14,16 @@ from torch_geometric.nn import SplineConv
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '../Retinotopy', 'data')
 pre_transform = T.Compose([T.FaceToEdge()])
-
 hemisphere = 'Left'  # or 'Right'
-
+norm_value = 70.4237
 
 # Loading test dataset
-dev_dataset = Retinotopy(path, 'Development', transform=T.Cartesian(max_value=10),
+dev_dataset = Retinotopy(path, 'Development', transform=T.Cartesian(max_value=norm_value),
                           pre_transform=pre_transform, n_examples=181,
                           prediction='polarAngle', myelination=True,
                           hemisphere=hemisphere)
 test_loader = DataLoader(dev_dataset, batch_size=1, shuffle=False)
-test_dataset = Retinotopy(path, 'Test', transform=T.Cartesian(max_value=10),
+test_dataset = Retinotopy(path, 'Test', transform=T.Cartesian(max_value=norm_value),
                           pre_transform=pre_transform, n_examples=181,
                           prediction='polarAngle', myelination=True,
                           hemisphere=hemisphere)
@@ -124,7 +123,7 @@ for i in range(5):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = Net().to(device)
     model.load_state_dict(
-        torch.load('./output/deepRetinotopy_PA_LH_model' + str(i+1) + '_fixNorm.pt',
+        torch.load('./output/deepRetinotopy_PA_LH_model' + str(i+1) + '.pt',
                    map_location=device))
 
     # Create an output folder if it doesn't already exist
@@ -156,4 +155,4 @@ for i in range(5):
                osp.join(osp.dirname(osp.realpath(__file__)),
                         'testset_results',
                         'testset-intactData_model' + str(
-                            i + 1) + '_fixNorm.pt'))
+                            i + 1) + '.pt'))
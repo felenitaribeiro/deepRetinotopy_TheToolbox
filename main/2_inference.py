edd337f4823af619f1c41917652e49e3a775f115
Fernanda Ribeiro <33485379+felenitaribeiro@users.noreply.github.com>
2024-05-01 17:55:56 +1000

    executable files (#26)
    * executable files
    
    * minor edit to path

--- main/2_inference.py
+++ main/2_inference.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import os.path as osp
 import torch
@@ -8,7 +10,7 @@ import numpy as np
 import argparse
 import time
 
-sys.path.append('..')
+sys.path.append(osp.dirname(osp.realpath(__file__))+'/..')
 
 from utils.rois import ROI_WangParcelsPlusFovea as roi
 from utils.model import deepRetinotopy
